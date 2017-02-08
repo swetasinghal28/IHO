9812e16952bfab8bcd52f57a57d543411148eb71
swetasinghal28 <ssing134@asu.edu>
2017-02-07 20:57:18 -0700

    Updating iOS app to SWIFT 3

---
+++ Code/SWIFT/IHO-ASU/IHO-ASU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IHO-ASU
+//
+//  Created by Sweta Singhal on 2/7/17.
+//  Copyright © 2017 Sweta Singhal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
