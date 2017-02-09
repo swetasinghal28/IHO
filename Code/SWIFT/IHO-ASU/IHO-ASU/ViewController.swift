46561ab998f2dde37c83cb05049faa05e288ff1e
swetasinghal28 <ssing134@asu.edu>
2017-02-09 16:38:03 -0700

    iOS: Adding round corners to buttons

--- Code/SWIFT/IHO-ASU/IHO-ASU/ViewController.swift
+++ Code/SWIFT/IHO-ASU/IHO-ASU/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var donate: UIButton!
+    @IBOutlet weak var about: UIButton!
+    @IBOutlet weak var gallery: UIButton!
+    @IBOutlet weak var connect: UIButton!
+    @IBOutlet weak var field: UIButton!
+    @IBOutlet weak var news: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // button sytle
+        news.layer.cornerRadius = 15
+        about.layer.cornerRadius = 15
+        donate.layer.cornerRadius = 15
+        gallery.layer.cornerRadius = 15
+        connect.layer.cornerRadius = 15
+        field.layer.cornerRadius = 15
     }
 
     override func didReceiveMemoryWarning() {
