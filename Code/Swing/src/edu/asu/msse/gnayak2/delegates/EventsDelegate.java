2ecb910ab96cbf857580fff40a4f26739550c7bb
gnayak7 <gowthamnayak@gmail.com>
2017-02-19 16:08:28 -0700

    Delegate for events

---
+++ Code/Swing/src/edu/asu/msse/gnayak2/delegates/EventsDelegate.java
@@ -0,0 +1,8 @@
+package edu.asu.msse.gnayak2.delegates;
+
+import edu.asu.msse.gnayak2.models.Event;
+
+public interface EventsDelegate {
+	public void addEvent(Event event);
+	public void deleteEvent(Event event);
+}
