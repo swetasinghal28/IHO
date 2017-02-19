ed09b57b29948abba03dbdfc0b8e5e6672245c2e
gnayak7 <gowthamnayak@gmail.com>
2017-02-18 22:25:16 -0700

    clean up

--- Code/Swing/src/edu/asu/msse/gnayak2/models/EventsDelegate.java
+++ Code/Swing/src/edu/asu/msse/gnayak2/models/EventsDelegate.java
@@ -1,6 +1,6 @@
 package edu.asu.msse.gnayak2.models;
 
-public interface EventDelegate {
+public interface EventsDelegate {
 	public void addEvent(Event event);
 	public void deleteEvent(Event event);
 }
