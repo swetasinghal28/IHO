b3089592dca5a2205f1d1ab9fed98bb6c6f2dcf6
gnayak7 <gowthamnayak@gmail.com>
2017-02-19 16:09:24 -0700

    Delegate for lectures

---
+++ Code/Swing/src/edu/asu/msse/gnayak2/delegates/LecturesDelegate.java
@@ -0,0 +1,8 @@
+package edu.asu.msse.gnayak2.delegates;
+
+import edu.asu.msse.gnayak2.models.Lecture;
+
+public interface LecturesDelegate {
+	public void addLecture(Lecture lecture);
+	public void deleteLecture(Lecture lecture);
+}
