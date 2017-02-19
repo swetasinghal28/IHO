0fb2ad553a2c72be89af0fe27cf9eafe36f31f27
gnayak7 <gowthamnayak@gmail.com>
2017-02-19 16:12:44 -0700

    News and Events - News Frame, Events Frame fully functional || Field Notes - Lectures, New Science Frames fully functional

--- Code/Swing/src/edu/asu/msse/gnayak2/models/EventsDelegate.java
+++
@@ -1,6 +0,0 @@
-package edu.asu.msse.gnayak2.models;
-
-public interface EventsDelegate {
-	public void addEvent(Event event);
-	public void deleteEvent(Event event);
-}
--- Code/Swing/src/edu/asu/msse/gnayak2/models/NewsDelegate.java
+++
@@ -1,6 +0,0 @@
-package edu.asu.msse.gnayak2.models;
-
-public interface NewsDelegate {
-	public void addNews(News news);
-	public void deleteNews(News news);
-}
