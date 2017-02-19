c300893739c0816440667ac126ee2f3582904b88
gnayak7 <gowthamnayak@gmail.com>
2017-02-19 16:10:00 -0700

    Delegate for new sciences

---
+++ Code/Swing/src/edu/asu/msse/gnayak2/delegates/NewSciencesDelegate.java
@@ -0,0 +1,8 @@
+package edu.asu.msse.gnayak2.delegates;
+
+import edu.asu.msse.gnayak2.models.NewScience;
+
+public interface NewSciencesDelegate {
+	public void addNewScience(NewScience newScience);
+	public void deleteNewScience(NewScience newScience);
+}
