7fdf8099db8468e0defa3732b853537e8b6c7f4d
gnayak7 <gowthamnayak@gmail.com>
2017-02-19 16:08:58 -0700

    Delegate for News

---
+++ Code/Swing/src/edu/asu/msse/gnayak2/delegates/NewsDelegate.java
@@ -0,0 +1,8 @@
+package edu.asu.msse.gnayak2.delegates;
+
+import edu.asu.msse.gnayak2.models.News;
+
+public interface NewsDelegate {
+	public void addNews(News news);
+	public void deleteNews(News news);
+}
