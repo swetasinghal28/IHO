7a47c006bc221025943ba8d98f95c4e452434dca
Arpit Jaiswal <ajaiswa2@asu.edu>
2017-03-14 11:39:02 -0700

    iOS: Adding Conenct View Controller

---
+++ Code/SWIFT/IHO-ASU/IHO-ASU/ConnectViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ConnectViewController.swift
+//  IHO-ASU
+//
+//  Created by Sweta Singhal on 2/9/17.
+//  Copyright © 2017 Sweta Singhal. All rights reserved.
+//
+
+import UIKit
+
+class ConnectViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        
+        self.navigationItem.title = "Connect"
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
