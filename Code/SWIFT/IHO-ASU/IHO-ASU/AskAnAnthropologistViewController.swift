81e278052c6d94c1deb2bb39c81226d9092dfd86
swetasinghal28 <ssing134@asu.edu>
2017-03-16 00:24:28 -0700

    Ignoring all DS_Store files

---
+++ Code/SWIFT/IHO-ASU/IHO-ASU/AskAnAnthropologistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AskAnAnthropologistViewController.swift
+//  IHO-ASU
+//
+//  Created by Sweta Singhal on 3/14/17.
+//  Copyright © 2017 Sweta Singhal. All rights reserved.
+//
+
+import UIKit
+
+class AskAnAnthropologistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
