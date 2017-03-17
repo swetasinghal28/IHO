de2f5b6878ca7d86de42bb0781eb2fc34a8911ff
swetasinghal28 <ssing134@asu.edu>
2017-03-16 17:53:11 -0700

    iOS: Adding image to Ask an Anthropologist

--- Code/SWIFT/IHO-ASU/IHO-ASU/AskAnAnthropologistViewController.swift
+++ Code/SWIFT/IHO-ASU/IHO-ASU/AskAnAnthropologistViewController.swift
@@ -9,37 +9,31 @@
 import UIKit
 
 class AskAnAnthropologistViewController: UIViewController, UITextViewDelegate {
-    @IBAction func anthropologistLink(_ sender: Any) {
+    
+    
+    @IBAction func visitAnthropologist(_ sender: Any) {
+        
+        
         let url = URL(string: "https://askananthropologist.asu.edu/")!
         if #available(iOS 10.0, *) {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         } else {
             UIApplication.shared.openURL(url)
         }
-
         
+
     }
     
-    @IBOutlet weak var textView: UITextView!
+    
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         
         self.navigationItem.title = "Ask An Anthropologist"
         
-        
-        let linkAttributes = [
-            NSLinkAttributeName: NSURL(string: "https://askabiologist.asu.edu")!,
-            NSForegroundColorAttributeName: UIColor.blue
-            ] as [String : Any]
-        
-        let attributedString = NSMutableAttributedString(string: "AskAnAnthropologist— inspired by the “wildly” successful AskABiologist.asu. edu—is a resource where students and teachers can  nd information about the science of anthropology. Aimed primarily at the middle school level, the articles and activities are challenging and may be used for higher school levels as well.")
-        
-        // Set the 'click here' substring to be the link
-        attributedString.setAttributes(linkAttributes, range: NSMakeRange(5, 10))
-        
-        self.textView.delegate = self
-        self.textView.attributedText = attributedString
 
     }
 
