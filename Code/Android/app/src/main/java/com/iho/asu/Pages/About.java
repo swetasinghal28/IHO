754e8f7c359bc0dc6d5888757eec0ffafbb5c864
Mihir Bhatt <mbbhatt@asu.edu>
2017-03-29 23:23:46 -0700

    connect homepage header fix + about fragment portrait

--- Code/Android/app/src/main/java/com/iho/asu/Pages/About.java
+++ Code/Android/app/src/main/java/com/iho/asu/Pages/About.java
@@ -2,7 +2,6 @@ package com.iho.asu.Pages;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,11 +17,10 @@ public class About extends Fragment{
                              ViewGroup container, Bundle savedInstanceState) {
         View v =  inflater.inflate(
                 R.layout.fragment_about, container, false);
-        String value = "<a href=\"https://iho.asu.edu/\">The Institute of Human Origins </a>is one of the preeminent research organizations in the world devoted to the science of human origins. IHO pursues an integrative strategy for research and discovery central to its over 30-year-old founding mission, bridging social, earth, and life science approaches to the most important questions concerning the course, causes, and timing of events in the human career over deep time. IHO fosters public awareness of human origins and its relevance to contemporary society through innovative outreach programs that create timely, accurate information for both education and lay communities."
-                +"The Institute of Human Origins is a research center of the <a href=\"https://clas.asu.edu/\">College of Liberal Arts and Sciences</a> in the <a href=\"https://shesc.asu.edu/\">School of Human Evolution and Social Change</a> at Arizona State University.</string>";
-
-        TextView t2 = (TextView) v.findViewById(R.id.highlightText);
-        t2.setText(Html.fromHtml(value));
+        //String value = "<a href=\"https://iho.asu.edu/\">The Institute of Human Origins </a>is one of the preeminent research organizations in the world devoted to the science of human origins. IHO pursues an integrative strategy for research and discovery central to its over 30-year-old founding mission, bridging social, earth, and life science approaches to the most important questions concerning the course, causes, and timing of events in the human career over deep time. IHO fosters public awareness of human origins and its relevance to contemporary society through innovative outreach programs that create timely, accurate information for both education and lay communities."
+          //      +"The Institute of Human Origins is a research center of the <a href=\"https://clas.asu.edu/\">College of Liberal Arts and Sciences</a> in the <a href=\"https://shesc.asu.edu/\">School of Human Evolution and Social Change</a> at Arizona State University.</string>";
+        //String value = R.string.aboutInfo;
+        TextView t2 = (TextView) v.findViewById(R.id.aboutInfoView);
         t2.setMovementMethod(LinkMovementMethod.getInstance());
         return v;
     }
