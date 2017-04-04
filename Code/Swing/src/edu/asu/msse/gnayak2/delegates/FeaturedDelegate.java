4ae7fd80881ad1efc7ef3dee7d85cae63993ade2
sindhu <sbalas24@asu.edu>
2017-04-03 23:36:47 -0700

    Implementing featured news

---
+++ Code/Swing/src/edu/asu/msse/gnayak2/delegates/FeaturedDelegate.java
@@ -0,0 +1,8 @@
+package edu.asu.msse.gnayak2.delegates;
+
+import edu.asu.msse.gnayak2.models.FeaturedNews;
+
+public interface FeaturedDelegate {
+	public void addNews(FeaturedNews news);
+	public void deleteNews(FeaturedNews news);
+}
