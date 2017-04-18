21dce0be845b14826fef119b9692c45c7205396e
sindhu <sbalas24@asu.edu>
2017-04-17 21:35:13 -0700

    Adding image preview in news

--- Code/Swing/src/edu/asu/msse/gnayak2/bl/EditNewsFrame.java
+++ Code/Swing/src/edu/asu/msse/gnayak2/bl/EditNewsFrame.java
@@ -1,7 +1,7 @@
 package edu.asu.msse.gnayak2.bl;
 
 import java.awt.Dimension;
-
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.HashMap;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -26,6 +27,7 @@ import edu.asu.msse.gnayak2.models.News;
 import edu.asu.msse.gnayak2.networking.HTTPConnectionHelper;
 import net.miginfocom.swing.MigLayout;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -57,7 +59,7 @@ public class EditNewsFrame extends JFrame {
 	String encodedImage;
 	
 	byte[] imageInByte;
-	
+	int flag = 0;
 	/**
 	 * Create the frame.
 	 */
@@ -113,9 +115,40 @@ public class EditNewsFrame extends JFrame {
 		taDescription.setText(news.getDesc());
 		tfLink.setText(news.getLink());
 		tfDate.setText(news.getDate());
-		//imageFileButton.setText(news.getDate());
+	    imageFileButton.setText(news.getImage());
+	    BufferedImage bufferedImage = convertStringToImage(news.getImage());
+		ImageIcon imageIcon = null;
+		 
+        if (bufferedImage != null) { 
+        	 imageIcon = new ImageIcon(bufferedImage);
+        	 Image oldImage = imageIcon.getImage(); // transform it 
+        	 Image newImage = oldImage.getScaledInstance(180, 180,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+        	 imageIcon = new ImageIcon(newImage); 
+        	 JLabel imageLabel = new JLabel(imageIcon);
+        	 panel.add(imageLabel);
+        	 
+        }
+        
+        
+        setPreferredSize(new Dimension(180,100));
 	}
  	
+	private BufferedImage convertStringToImage(String base64String) {
+    	BufferedImage image = null;
+    	byte[] imageByte;
+    	imageByte = Base64.getDecoder().decode(base64String);
+    	ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+    	try {
+			image = ImageIO.read(bis);
+			bis.close();
+			return image;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}     	
+
+    }
 	public EditNewsFrame(NewsDelegate newsdelegate) {
 		this.newsDelegate = newsdelegate;
 		setUpFrame();
@@ -126,7 +159,7 @@ public class EditNewsFrame extends JFrame {
 		btnSubmit.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+            if(flag == 1 ){
 			   try{
 
 		 	BufferedImage originalImage =
@@ -143,18 +176,22 @@ public class EditNewsFrame extends JFrame {
            	}catch(IOException e1){
 		 		System.out.println(e1.getMessage());
 			 	}
+            }
 //     			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 //				Date date = new Date();
 //				System.out.println(dateFormat.format(date)); 
+            if(flag == 1) {
 			   System.out.println("DATE-----" + tfDate.getText());
 			   
 				
 				News newNews = new News(tfTitle.getText(), taDescription.getText(),tfLink.getText(), tfDate.getText(), encodedImage);
-				// delete old news
-			//	System.out.println("BYTE_STRING_________"+imageInByte.toString());
-				
-				
 				newsDelegate.addNews(newNews);
+            }
+            else {
+            	   System.out.println("DATE-----" + tfDate.getText());
+   				News newNews = new News(tfTitle.getText(), taDescription.getText(),tfLink.getText(), tfDate.getText(), imageFileButton.getText());
+   				newsDelegate.addNews(newNews);
+            }
 				if (news != null){
 					newsDelegate.deleteNews(news);
 				}
@@ -172,7 +209,7 @@ public class EditNewsFrame extends JFrame {
 					    File f = chooser.getSelectedFile();
 					    filename = f.getAbsolutePath();
 					    imageFileButton.setText(filename);
-					    
+					    flag=1;
 
 					
 				}
